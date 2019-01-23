2b69f8c8eb8e7ee1a12db999bbaa8f0910fb45e7
Clement Barbaza <clement.barbaza@gmail.com>
2019-01-23 18:01:21 +0100

    Init

---
+++ example/index.php
@@ -0,0 +1,7 @@
+<?php
+
+require __DIR__ . "/../vendor/autoload.php";
+
+use CommonSense\CommonSense;
+
+new CommonSense; // Does nothing. Just a frienly reminder :)
---
+++ src/CommonSense.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace CommonSense;
+
+/**
+ * CommonSense
+ * A friendly reminder to never forget to use common sense when coding a PHP project.
+ */
+class CommonSense
+{
+
+}
